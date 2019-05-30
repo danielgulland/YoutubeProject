511f993cb628fff883ffb0022aae36a38eded596
NateTheGreat123 <nathanjsmith95@gmail.com>
2019-05-29 18:34:29 -0700

    delete user
    Resolves #27

--- src/test/java/app/service/UserServiceTest.java
+++ src/test/java/app/service/UserServiceTest.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-//import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,13 +27,13 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-//import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 
    private static final int USER_ID = 1;
+   private static final int INVALID_ID = 0;
    private static final String USERNAME = "testUser";
    private static final String EMAIL = "test@email.com";
    private static final String PASSWORD = "password";
@@ -392,6 +391,42 @@ public class UserServiceTest {
          Assert.assertTrue(ex.getFields().contains("oldPassword"));
          Assert.assertNotNull(updateUserData.getPassword());
       }
+   }
+
+   @Test
+   public void testDeleteUserById_validID() {
+      //Arrange
+      final User user = buildUser();
+      when(userDao.findById(USER_ID)).thenReturn(Optional.of(user));
+
+      //Act
+      userService.deleteUserById(USER_ID);
+
+      //Assert
+      verify(userDao).findById(USER_ID);
+      verify(userDao).deleteById(USER_ID);
+      verifyNoMoreInteractions(userDao);
+   }
+
+   @Test
+   public void testDeleteUserById_invalidID() {
+      //Arrange
+      when(userDao.findById(INVALID_ID)).thenReturn(Optional.empty());
+
+      //Act
+      try {
+         userService.deleteUserById(INVALID_ID);
+         fail("Exception not thrown");
+      } catch (ApiException ex) {
+         //Assert
+         verify(userDao).findById(INVALID_ID);
+         verifyNoMoreInteractions(userDao);
+
+         Assert.assertEquals(ValidationError.NOT_FOUND, ex.getError());
+         Assert.assertEquals("User does not exist", ex.getMessage());
+         Assert.assertEquals(1, ex.getFields().size());
+      }
+
    }
 
    private User buildUser() {
