7e1f8c5fc3b347b72af974c4412e96ea51ea27ec
danielgulland <dannygulland@gmail.com>
2019-06-16 00:56:20 -0700

    fixed true/false bug for createPlaylistData
    resolves #66

--- src/main/java/app/request/CreatePlaylistData.java
+++ src/main/java/app/request/CreatePlaylistData.java
@@ -2,10 +2,13 @@ package app.request;
 
 import lombok.Data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Data
 public class CreatePlaylistData {
    private String name;
    private int userId;
+   @JsonProperty
    private boolean isPrivate;
    private String genre;
 }
