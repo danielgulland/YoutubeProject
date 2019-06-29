fd142da886c04773066a613b49da9a2b5d507441
danielgulland <dannygulland@gmail.com>
2019-06-28 20:09:23 -0700

    created sql table and started creating model

---
+++ src/main/java/app/model/Room.java
@@ -0,0 +1,25 @@
+package app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Room")
+public class Room {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Integer id;
+}
