cb614aff35fbc480ae6ef70b517a0a3e2a2d2ff5
NateTheGreat123 <nathanjsmith95@gmail.com>
2019-07-20 18:44:27 -0700

    Get Rooms
    Resolves #81

--- src/main/java/app/dao/RoomDao.java
+++ src/main/java/app/dao/RoomDao.java
@@ -2,8 +2,10 @@ package app.dao;
 
 import app.model.Room;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomDao extends JpaRepository<Room, Integer> {
-
+   List<Room> findByNameStartingWith(String room);
 }
